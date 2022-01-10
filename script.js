63db71356f971d7f0142272334093698ce55e8ab
Virag Kormoczy <vkormoczy@gmail.com>
2022-01-10 17:54:17 +0200

    Added night colors to background buildings and windows

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const body = document.querySelector("body");
+const nightWindow1 = document.querySelectorAll(".night-window1");
+const nightWindow2 = document.querySelectorAll(".night-window2");
+const nightWindow3 = document.querySelectorAll(".night-window3");
+const nightWindow4 = document.querySelectorAll(".night-window4");
+const nightWindow5 = document.querySelectorAll(".night-window5");
+const nightWindow6 = document.querySelectorAll(".night-window6");
+const nightBuilding = document.querySelectorAll(".night-building");
 
 body.addEventListener("click", (e) => {
   body.classList.toggle("night");
+  nightWindow1.forEach((window) => window.classList.toggle("night-window1"));
+  nightWindow2.forEach((window) => window.classList.toggle("night-window2"));
+  nightWindow3.forEach((window) => window.classList.toggle("night-window3"));
+  nightWindow4.forEach((window) => window.classList.toggle("night-window4"));
+  nightWindow5.forEach((window) => window.classList.toggle("night-window5"));
+  nightWindow6.forEach((window) => window.classList.toggle("night-window6"));
+  nightBuilding.forEach((building) =>
+    building.classList.toggle("night-building")
+  );
 });
